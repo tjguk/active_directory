d7299babd28ef550c9a23dfe864c88398f73b5cf
Tim Golden <mail@timgolden.me.uk>
2010-03-16 17:51:35 +0000

    Create BigGroup
    modified   big-test.py

--- trials/big-test.py
+++ trials/big-test.py
@@ -3,9 +3,8 @@ import os, sys
 import active_directory as ad
 
 def main ():
-  big_ou = ad.find_ou ("BigOU")
-  if big_ou is None:
-    big_ou = ad.root ().new_ou ("BigOU")
+  big_ou = ad.find_ou ("BigOU") or ad.root ().new_ou ("BigOU")
+  big_group = big_ou.find_group ("BigGroup") or big_ou.new ("group", "BigGroup")
 
 if __name__ == '__main__':
   main (*sys.argv[1:])
