fa5621e45b91dd9d72348fd0b2dc64c29c09126b
Tim Golden <mail@timgolden.me.uk>
2010-03-16 17:56:38 +0000

    Special-case group creation
    modified   big-test.py

--- trials/big-test.py
+++ trials/big-test.py
@@ -4,7 +4,7 @@ import active_directory as ad
 
 def main ():
   big_ou = ad.find_ou ("BigOU") or ad.root ().new_ou ("BigOU")
-  big_group = big_ou.find_group ("BigGroup") or big_ou.new ("group", "BigGroup")
+  big_group = big_ou.find_group ("BigGroup") or big_ou.new_group ("BigGroup")
 
 if __name__ == '__main__':
   main (*sys.argv[1:])
