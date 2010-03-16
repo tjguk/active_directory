da171b54e379a9f0f16bb1ca88111ea7a845cb3b
Tim Golden <mail@timgolden.me.uk>
2010-03-16 18:42:05 +0000

    Increase the number of test users
    modified   big-test.py

--- trials/big-test.py
+++ trials/big-test.py
@@ -5,7 +5,7 @@ import active_directory as ad
 def main ():
   big_ou = ad.root ().find_ou ("BigOU") or ad.root ().new_ou ("BigOU")
   big_group = big_ou.find_group ("BigGroup") or big_ou.new_group ("BigGroup")
-  for i in range (3000):
+  for i in range (4000):
     username = "user%04d" % i
     user = big_ou.find_user (username)
     if not user:
