cf87ce9790b1635616dbd36e5131c4c9ba07001f
Tim Golden <mail@timgolden.me.uk>
2011-05-25 12:48:39 +0000

    Repurpose test_accounts as a template module, ready for copying

---
+++ active_directory2/tests/test.template.py
@@ -0,0 +1,22 @@
+import os, sys
+import unittest as unittest0
+try:
+  unittest0.skipUnless
+  unittest0.skip
+except AttributeError:
+  import unittest2 as unittest
+else:
+  unittest = unittest0
+del unittest0
+
+from active_directory2 import core
+from active_directory2.tests import utils
+
+class TestTemplate (unittest.TestCase):
+
+  def setUp (self):
+    pass
+
+  def tearDown (self):
+    pass
+
