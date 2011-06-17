e47e574c19112558639b8a8a7431f6367228ea5e
Tim Golden <mail@timgolden.me.uk>
2011-06-17 09:44:47 +0000

    Added threaded sample

---
+++ trials/threaded.py
@@ -0,0 +1,26 @@
+import os, sys
+import logging
+import Queue
+import threading
+
+from active_directory2 import ad
+ad.logger.setLevel (logging.INFO)
+
+def f (ident, filter, queue):
+  for item in ad.AD ().search (filter):
+    queue.put ((ident, item.dn))
+  queue.put ((ident, "***"))
+
+if __name__ == '__main__':
+  q = Queue.Queue ()
+  t1 = threading.Thread (target=f, args=('users', 'objectClass=user', q))
+  t2 = threading.Thread (target=f, args=('groups', 'objectClass=group', q))
+  t1.start ()
+  t2.start ()
+  incomplete = dict (users=True, groups=True)
+  while any (incomplete.values ()):
+    ident, dn = q.get ()
+    if dn == "***":
+      incomplete[ident] = False
+    else:
+      print ident, ":", dn
