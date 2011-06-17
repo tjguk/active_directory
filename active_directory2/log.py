cff71f7db0290a7d55a4c5dceb049eb3e04e9b10
Tim Golden <mail@timgolden.me.uk>
2011-06-17 10:40:30 +0000

    Send debug info to log file by default Send warnings+ to stderr by default

--- active_directory2/log.py
+++ active_directory2/log.py
@@ -1,10 +1,16 @@
 import os, sys
 import logging
 
-formatter = logging.Formatter ("%(levelname)s: %(module)s.%(funcName)s - %(message)s")
+formatter = logging.Formatter ("[%(levelname)s] %(module)s.%(funcName)s: %(message)s")
 logger = logging.getLogger ("active_directory2")
 logger.setLevel (logging.DEBUG)
+
 stderr_handler = logging.StreamHandler (sys.stderr)
-stderr_handler.setLevel (logging.DEBUG)
+stderr_handler.setLevel (logging.WARN)
 stderr_handler.setFormatter (formatter)
 logger.addHandler (stderr_handler)
+
+debug_handler = logging.FileHandler ("active_directory2.debug.log")
+debug_handler.setLevel (logging.DEBUG)
+debug_handler.setFormatter (formatter)
+logger.addHandler (debug_handler)
