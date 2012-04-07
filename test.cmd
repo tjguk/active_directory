e442a5a29c21a2a172321c50036d551cf9f13705
Tim Golden <mail@timgolden.me.uk>
2012-04-07 19:25:08 +0100

    Remove *.pyc before starting the test run, just in case... Get ready for PEP8-ness later

--- test.cmd
+++ test.cmd
@@ -2,5 +2,7 @@ REM
 REM Run tests for all available versions of Python between 2.3 & 3.3
 REM
 @echo off
-for /l %%n in (23,1,33) do if exist c:\python%%n\python.exe (echo. & echo python%%n & c:\python%%n\python.exe -W ignore test_active_directory.py)
+del /s/q *.pyc
+REM python -mpep8 --max-line-length=120 --ignore=E401,E501,W292,E302,E261 .
+for /l %%n in (24,1,33) do if exist c:\python%%n\python.exe (echo. & echo python%%n & c:\python%%n\python.exe -W ignore test_active_directory.py)
 pause
