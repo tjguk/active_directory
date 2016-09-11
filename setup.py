ec0b4f2c2366f2b62e75c557a7f9956810e8ead5
Tim Golden <mail@timgolden.me.uk>
2016-09-11 20:59:29 +0100

    Prep for upload

--- setup.py
+++ setup.py
@@ -1,40 +1,45 @@
-# -*- coding: UTF8 -*-
-from distutils.core import setup
+#
+# Initially copied from:
+# https://raw.githubusercontent.com/pypa/sampleproject/master/setup.py
+#
+
+from setuptools import setup, find_packages
+import os
+import codecs
 import __active_directory_version__ as __version__
 
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Win32 (MS Windows)',
-    'Intended Audience :: Developers',
-    'Intended Audience :: System Administrators',
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 3",
-    'License :: PSF',
-    'Natural Language :: English',
-    'Operating System :: Microsoft :: Windows :: Windows 95/98/2000',
-    'Topic :: System :: Systems Administration'
-]
+here = os.path.abspath(os.path.dirname(__file__))
 
-#
-# setup wants a long description which we'd like to read
-# from README.rst; setup also wants a file called README
-# github, however, wants a file called readme.rst. This
-# is the compromise:
-#
-try:
-    long_description = open("README.rst").read()
-    open("README", "w").write(long_description)
-except (OSError, IOError):
-    long_description = ""
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="active_directory",
-    version=__version__.__VERSION__ + __version__.__RELEASE__,
-    description="Active Directory",
-    author="Tim Golden",
-    author_email="mail@timgolden.me.uk",
-    url="https://github.com/tjguk/active_directory",
-    license="http://www.opensource.org/licenses/mit-license.php",
-    py_modules=["active_directory", "__active_directory_version__"],
+    name='active_directory',
+
+    version=__version__,
+
+    description='Active Directory',
     long_description=long_description,
+
+    url='https://github.com/tjguk/active_directory',
+
+    author='Tim Golden',
+    author_email='mail@timgolden.me.uk',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        'License :: PSF',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows :: Windows 95/98/2000',
+        'Topic :: System :: Systems Administration'
+    ],
+
+    py_modules=["active_directory", "__active_directory_version__"],
 )
